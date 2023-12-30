fb835b70755015c4a035473e2b5b370e46d48c4a
Chandresh patidar <patidarchandresh2002@gmail.com>
2023-12-30 09:10:28 +0530

    feat: add link to home page on logo

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,17 +1,26 @@
 <script>
-    import { reveal ,setDefaultOptions} from 'svelte-reveal';
-    	
-	
+  import { reveal, setDefaultOptions } from 'svelte-reveal';
 </script>
-<nav class="w-full container flex justify-between left-1/2 z-10 -translate-x-1/2 mx-auto fixed py-8 px-12">
-    <figure  class="font-medium">Chandresh patidar</figure>
 
-    <ul   class="flex gap-6">
-        <a href="/coming-soon"><li class="font-semibold text-gray-600 hover:text-black ">Projects</li></a>
-        <a href="/coming-soon"><li class="font-semibold text-gray-600 hover:text-black">Open Source</li></a>
-        <a href="/coming-soon"><li class="font-semibold text-gray-600 hover:text-black">Blogs</li></a>
-    </ul>
-    <button class="text-lg font-medium border-b text-black tracking-tight">
-        chandresh.dev@gmail.com
-    </button>
+<nav
+  class="w-full container flex justify-between left-1/2 z-10 -translate-x-1/2 mx-auto fixed py-8 px-12"
+>
+  <a href="/">
+    <figure class="font-medium">Chandresh patidar</figure>
+  </a>
+
+  <ul class="flex gap-6">
+    <a href="/coming-soon">
+      <li class="font-semibold text-gray-600 hover:text-black">Projects</li>
+    </a>
+    <a href="/coming-soon">
+      <li class="font-semibold text-gray-600 hover:text-black">Open Source</li>
+    </a>
+    <a href="/coming-soon">
+      <li class="font-semibold text-gray-600 hover:text-black">Blogs</li>
+    </a>
+  </ul>
+  <button class="text-lg font-medium border-b text-black tracking-tight">
+    chandresh.dev@gmail.com
+  </button>
 </nav>
